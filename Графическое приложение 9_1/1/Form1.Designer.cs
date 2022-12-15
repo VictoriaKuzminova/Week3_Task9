cc9b325a83ecdfb725d89b03e1cf88ab89fb0e82
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-12-15 12:45:26 +0300

    Add files via upload

---
+++ Графическое приложение 9_1/1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mas_textBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.n_textBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.start_textBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.finish_textBox = new System.Windows.Forms.TextBox();
+            this.res_textBox = new System.Windows.Forms.RichTextBox();
+            this.res_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mas_textBox
+            // 
+            this.mas_textBox.Location = new System.Drawing.Point(290, 72);
+            this.mas_textBox.Name = "mas_textBox";
+            this.mas_textBox.Size = new System.Drawing.Size(449, 103);
+            this.mas_textBox.TabIndex = 11;
+            this.mas_textBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Последовательность: ";
+            // 
+            // n_textBox
+            // 
+            this.n_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.n_textBox.Location = new System.Drawing.Point(592, 18);
+            this.n_textBox.Name = "n_textBox";
+            this.n_textBox.Size = new System.Drawing.Size(147, 30);
+            this.n_textBox.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(530, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Введите количество элементов последовательности: \r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 25);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Диапазон: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(200, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 25);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "от";
+            // 
+            // start_textBox
+            // 
+            this.start_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.start_textBox.Location = new System.Drawing.Point(290, 206);
+            this.start_textBox.Name = "start_textBox";
+            this.start_textBox.Size = new System.Drawing.Size(147, 30);
+            this.start_textBox.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(521, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 25);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "до";
+            // 
+            // finish_textBox
+            // 
+            this.finish_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.finish_textBox.Location = new System.Drawing.Point(592, 201);
+            this.finish_textBox.Name = "finish_textBox";
+            this.finish_textBox.Size = new System.Drawing.Size(147, 30);
+            this.finish_textBox.TabIndex = 16;
+            // 
+            // res_textBox
+            // 
+            this.res_textBox.Location = new System.Drawing.Point(17, 327);
+            this.res_textBox.Name = "res_textBox";
+            this.res_textBox.Size = new System.Drawing.Size(722, 136);
+            this.res_textBox.TabIndex = 18;
+            this.res_textBox.Text = "";
+            // 
+            // res_button
+            // 
+            this.res_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.res_button.Location = new System.Drawing.Point(281, 253);
+            this.res_button.Name = "res_button";
+            this.res_button.Size = new System.Drawing.Size(161, 40);
+            this.res_button.TabIndex = 17;
+            this.res_button.Text = "Вывести";
+            this.res_button.UseVisualStyleBackColor = true;
+            this.res_button.Click += new System.EventHandler(this.res_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 488);
+            this.Controls.Add(this.res_textBox);
+            this.Controls.Add(this.res_button);
+            this.Controls.Add(this.finish_textBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.start_textBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.mas_textBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.n_textBox);
+            this.Controls.Add(this.label2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Работа с бинарными файлами";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox mas_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox n_textBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox start_textBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox finish_textBox;
+        private System.Windows.Forms.RichTextBox res_textBox;
+        private System.Windows.Forms.Button res_button;
+    }
+}
+
---
+++ Графическое приложение 9_1/1/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        private int n;
+        private int[] arr;
+        private int a, b;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void res_button_Click(object sender, EventArgs e)
+        {
+            if (n_textBox.Text == "" || start_textBox.Text == "" || finish_textBox.Text == "" || mas_textBox.Text == "")
+            {
+                MessageBox.Show("Введены не все данные!!!");
+                return;
+            }
+            else
+            {
+                res_textBox.Text = "";
+                try
+                {
+                    n = int.Parse(n_textBox.Text);
+                    if (n <= 0)
+                    {
+                        MessageBox.Show("Минимальное число 1");
+                        return;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Некорректный ввод");
+                    return;
+                }
+                arr = new int[n];
+                try
+                {
+                    var digits = mas_textBox.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (digits.Length > n)
+                    {
+                        MessageBox.Show("Число введенных элементов больше, чем количество элементов массива!");
+                        return;
+                    }
+                    else if (digits.Length < n)
+                    {
+                        MessageBox.Show("Число введенных элементов меньше, чем количество элементов массива!");
+                        return;
+                    }
+                    for (int i = 0; i < n; ++i)
+                    {
+                        arr[i] = int.Parse(digits[i]);
+                    }
+                }
+
+                catch
+                {
+                    MessageBox.Show("В поле элементов массива введены неверные значения! Введите заново, используя только цифры, точку и знак \"минус\"");
+                    return;
+                }
+
+                try
+                {
+                    a = int.Parse(start_textBox.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Некорректный ввод!");
+                    return;
+                }
+                try
+                {
+                    b = int.Parse(finish_textBox.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Некорректный ввод!");
+                    return;
+                }
+                if (a > b)
+                {
+                    MessageBox.Show("Начальная точка диапазона не может быть больше конечной точки");
+                    return;
+                }
+                FileStream f = new FileStream("text.dat", FileMode.Create);
+                BinaryWriter fOut = new BinaryWriter(f);
+                for (int i = 0; i < n; ++i)
+                {
+                    fOut.Write(arr[i]);
+                }
+                fOut.Close();
+                f.Close();
+                f = new FileStream("text.dat", FileMode.Open);
+                BinaryReader fIn = new BinaryReader(f);
+                long m = f.Length;
+                int am;
+                for (long i = 0; i < m; i += 4)
+                {
+                    f.Seek(i, SeekOrigin.Begin);
+                    am = fIn.ReadInt32();
+                    if ((am >= a) && (am <= b))
+                    {
+                        res_textBox.Text += am + " ";
+                    }
+                }
+                fIn.Close();
+                f.Close();
+            }
+        }
+    }
+}
