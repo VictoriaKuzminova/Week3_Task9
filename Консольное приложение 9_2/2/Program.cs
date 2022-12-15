43da9d4b9c6384e94308f6a7b34a9e94a240d7e0
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-12-15 12:22:45 +0300

    Add files via upload

---
+++ Консольное приложение 9_2/2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            while (true)
+            {
+                try {
+                    Console.Write("Введите букву: ");
+                    ch = Convert.ToChar(Console.Read());
+                    break;
+                }
+                catch { Console.WriteLine("Некорректный ввод данных"); }
+            }
+            const string path = "text.txt";
+            Console.WriteLine("Найденные строки по критерию: ");
+            int a = 0;
+            foreach (var str in File.ReadAllLines(path))
+            {
+                if (str.StartsWith(ch.ToString(), StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine(str);
+                    a++;
+                    continue;
+                }
+
+            }
+            if (a >= 1)
+            {
+                Environment.Exit(0);
+            }
+            else
+             Console.WriteLine("Строка не найдена");
+            
+        }
+    }
+}
