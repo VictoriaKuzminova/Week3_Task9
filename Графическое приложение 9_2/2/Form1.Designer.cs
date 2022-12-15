da16990d2c7716a0ea542e43b35b666619bb0eb4
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-12-15 12:45:59 +0300

    Add files via upload

---
+++ Графическое приложение 9_2/2/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace _2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ch_TextBox = new System.Windows.Forms.TextBox();
+            this.res_Button = new System.Windows.Forms.Button();
+            this.res_TextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(22, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите символ:";
+            // 
+            // ch_TextBox
+            // 
+            this.ch_TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ch_TextBox.Location = new System.Drawing.Point(250, 47);
+            this.ch_TextBox.Name = "ch_TextBox";
+            this.ch_TextBox.Size = new System.Drawing.Size(167, 30);
+            this.ch_TextBox.TabIndex = 1;
+            // 
+            // res_Button
+            // 
+            this.res_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.res_Button.Location = new System.Drawing.Point(91, 112);
+            this.res_Button.Name = "res_Button";
+            this.res_Button.Size = new System.Drawing.Size(272, 41);
+            this.res_Button.TabIndex = 2;
+            this.res_Button.Text = "Вывести строки";
+            this.res_Button.UseVisualStyleBackColor = true;
+            this.res_Button.Click += new System.EventHandler(this.res_Button_Click);
+            // 
+            // res_TextBox
+            // 
+            this.res_TextBox.Location = new System.Drawing.Point(27, 187);
+            this.res_TextBox.Name = "res_TextBox";
+            this.res_TextBox.Size = new System.Drawing.Size(390, 149);
+            this.res_TextBox.TabIndex = 3;
+            this.res_TextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 360);
+            this.Controls.Add(this.res_TextBox);
+            this.Controls.Add(this.res_Button);
+            this.Controls.Add(this.ch_TextBox);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Работа с файлами";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ch_TextBox;
+        private System.Windows.Forms.Button res_Button;
+        private System.Windows.Forms.RichTextBox res_TextBox;
+    }
+}
+
---
+++ Графическое приложение 9_2/2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2
+{
+    public partial class Form1 : Form
+    {
+        private char ch;
+        private int a = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void res_Button_Click(object sender, EventArgs e)
+        {
+            if (ch_TextBox.Text == "")
+            {
+                MessageBox.Show("Введены не все значения!!!");
+            }
+            else
+            {
+                res_TextBox.Text = "";
+                try
+                {
+                    ch = Convert.ToChar(ch_TextBox.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Некорректный ввод данных");
+                    return;
+                }
+                const string path = "text.txt";
+                res_TextBox.Text += "Найденные строки по критерию: \n";
+                foreach (var str in File.ReadAllLines(path))
+                {
+                    if (str.StartsWith(ch.ToString(), StringComparison.OrdinalIgnoreCase))
+                    {
+                        res_TextBox.Text += str+"\n";
+                        a++;
+                        continue;
+                    }
+                }
+                if (a >= 1)
+                {
+                    return;
+                }
+                else
+                {
+                    res_TextBox.Text += "Строка не найдена";
+                }
+            }
+        }
+    }
+}
+
